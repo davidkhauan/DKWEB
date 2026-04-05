ea7a9bc12d07bf10a36eb8557310fe6e790fa6b4
davidkhauan <thalsmatsumira@gmail.com>
2026-04-05 11:42:37 -0300

    'beleza'

--- projects.js
+++ projects.js
@@ -1,6 +1,7 @@
 const images = {
     galeriaMosaico: "./assets/galeriaMosaico.png",
-    advocaciaGeral: "./assets/advogadoGeral.png"
+    advocaciaGeral: "./assets/advogadoGeral.png",
+    helenaBeleza: "./assets/helenaBeleza.png"
 }
 
 const Projects = [
@@ -24,10 +25,91 @@ const Projects = [
     },
     {
         id: 3,
+        title: "Helena Beleza",
+        description: "Salão de beleza com identidade visual moderna e responsiva.",
+        image: images.helenaBeleza,
+        tags: [],
+        link: "https://davidkhauan.github.io/BelezaSalao/",
+        isComingSoon: false
+    },
+    {
+        id: 4,
         title: "Em Desenvolvimento...",
         description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
         image: "",
-        tags: ["Projeto #3"],
+        tags: ["Projeto #4"],
+        link: "#",
+        isComingSoon: true
+    },
+    {
+        id: 5,
+        title: "Em Desenvolvimento...",
+        description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
+        image: "",
+        tags: ["Projeto #5"],
+        link: "#",
+        isComingSoon: true
+    },
+    {
+        id: 6,
+        title: "Em Desenvolvimento...",
+        description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
+        image: "",
+        tags: ["Projeto #6"],
+        link: "#",
+        isComingSoon: true
+    },
+    {
+        id: 7,
+        title: "Em Desenvolvimento...",
+        description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
+        image: "",
+        tags: ["Projeto #7"],
+        link: "#",
+        isComingSoon: true
+    },
+    {
+        id: 8,
+        title: "Em Desenvolvimento...",
+        description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
+        image: "",
+        tags: ["Projeto #8"],
+        link: "#",
+        isComingSoon: true
+    },
+    {
+        id: 9,
+        title: "Em Desenvolvimento...",
+        description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
+        image: "",
+        tags: ["Projeto #9"],
+        link: "#",
+        isComingSoon: true
+    },
+    {
+        id: 10,
+        title: "Em Desenvolvimento...",
+        description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
+        image: "",
+        tags: ["Projeto #10"],
+        link: "#",
+        isComingSoon: true
+    },
+    {
+        id: 11,
+        title: "Em Desenvolvimento...",
+        description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
+        image: "",
+        tags: ["Projeto #11"],
+        link: "#",
+        isComingSoon: true
+    },
+    {
+        id: 12,
+        title: "Em Desenvolvimento...",
+        description: "Novo projeto em construção. Fique atento, em breve estará disponível para visualização!",
+        image: "",
+        tags: ["Projeto #12"],
         link: "#",
         isComingSoon: true
     }
